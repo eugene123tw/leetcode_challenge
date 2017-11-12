d8b0b506af475f3a7f3a82925ac42dc1e949eaae
Eugene <eugene123tw@gmail.com>
2017-11-12 18:04:49 +0800

    update

--- _Linked List/cpp/21_mergeTwoLists.cpp
+++ _Linked List/cpp/21_mergeTwoLists.cpp
@@ -9,22 +9,62 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-int main(){
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* result = new ListNode(0);
+        ListNode* ptr = result;
 
-    int a[5] = {1,3,5,7,9};
-    int b[4] = {2,4,6,8};
+        while(l1!=NULL || l2!=NULL){
+            int x =  l1!=NULL?l1->val:9999999;
+            int y =  l2!=NULL?l2->val:9999999;
 
-    ListNode* l1(0);
+            ptr->next = new ListNode(x>y?y:x);
+            ptr = ptr->next;
 
-    ListNode* ptr = l1;
+            if(x>y){
+                l2 = l2->next;
+            }else{
+                l1 = l1->next;
+            }            
+        }
+        return result->next;
+    }
+};
 
-    for(auto i:a){
-        ListNode* newNode = new ListNode(i);
+ListNode* vector2ListNode(vector<int> *ar_ptr){
+    ListNode* list = new ListNode(0);
+    ListNode* ptr = list;
+    for(vector<int>::iterator i=ar_ptr->begin();i<ar_ptr->end();i++){
+        ListNode* newNode = new ListNode(*i);
         ptr->next = newNode;
         ptr = ptr->next;
     }
+    return list->next;
+}
 
-    cout<< l1->next->val <<endl;
+void printList(ListNode* inList){
+    
+    ListNode* ptr = inList;
+    while(ptr!=NULL){
+        cout<< ptr->val << endl;
+        ptr = ptr->next;
+    }
+}
+
+int main(){
+
+    vector<int> a = {1,3,5,7,9};
+    vector<int> b = {2,4,6,8};
+
+    ListNode* l1 = vector2ListNode(&a);
+    ListNode* l2 = vector2ListNode(&b);
+    ListNode* result;
+
+    Solution obj;
+    result = obj.mergeTwoLists(l1,l2);
+
+    printList(result);
 
     return 0;
 }
